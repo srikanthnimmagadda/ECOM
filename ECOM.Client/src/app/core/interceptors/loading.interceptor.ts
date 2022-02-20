0ac655d4a22370a3b0a44e00819cc37a0d0102ce
Srikanth Nimmagadda <srikanth.nimagada@gmail.com>
2022-02-21 00:54:53 +0530

    UI Changes and added breadcrumb

---
+++ ECOM.Client/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { delay, finalize, Observable } from 'rxjs';
+import { BusyService } from '../services/busy.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private busyService: BusyService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // if (request.method === 'POST' && request.url.includes('orders')) {
+    //   return next.handle(request);
+    // }
+    // if (request.method === 'DELETE') {
+    //   return next.handle(request);
+    // }
+    // if (request.url.includes('emailexists')) {
+    //   return next.handle(request);
+    // }
+    this.busyService.busy();
+    return next.handle(request).pipe(
+      delay(1000),
+      finalize(() => {
+        this.busyService.idle();
+      }));
+  }
+}
