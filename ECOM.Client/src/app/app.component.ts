007be254218756ce1bb7db1489e20c72f37b66bc
Srikanth Nimmagadda <srikanth.nimagada@gmail.com>
2022-02-25 22:08:30 +0530

    Added Basket Client Functionality and feature checkout module

--- ECOM.Client/src/app/app.component.ts
+++ ECOM.Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'ECOM.Client';
 
-  constructor() {
+  constructor(private basketService: BasketService) {
   }
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      })
+    }
   }
 }
